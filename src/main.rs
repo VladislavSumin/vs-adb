1829948c9edd0caddf49bff1d093eaf4f8c794a0
Владислав <vladislavsumin@mail.ru>
2024-06-12 12:32:01 +0300

    add AdbClient struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+}
+
+struct AdbClient {
+    host: String,
+    port: u16,
+}
+
+impl AdbClient {
+    fn new<T: ToString>(host: T, port: u16) -> Self {
+        Self {
+            host: host.to_string(),
+            port,
+        }
+    }
+}
+
+impl Default for AdbClient {
+    fn default() -> Self {
+        Self::new("localhost", 5037)
+    }
 }
